6fb23900e222b824f8f81236b91412da2a8b791d
Alexander Krendl <krendl.alexander@gmail.com>
2022-01-14 20:55:30 +0100

    new2

--- acceleration.c
+++ acceleration.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <time.h>
 
-#define ACCELERATION 9.80665 /* m/s */
+#define ACCELERATION 9.80665 /* m/s/s */
+#define SPEED_SOUND 343 /* m/s */
+#define SPEED_LIGHT 300000000 /* m/s */
+#define AU 
+#define RUN_TIME 3600
 
 int main()
 {
     double speed = 0;
     double distance_covered = 0;
+    double speed_kmh, speed_mach, speed_light;
     int i;
-    for (i = 0; i < 60; i++){
-        printf("Speed: %f, Distance: %f.\n", speed += ACCELERATION, distance_covered += speed);
-        time.sleep(1);
+    for (i = 1; i <= RUN_TIME; i++) {
+
+        speed += ACCELERATION;
+        speed_kmh = speed * 3.6;
+        speed_mach = speed / SPEED_SOUND;
+        speed_light = speed / SPEED_LIGHT;
+        distance_covered += speed;
+        distance_au = distance_covered /
+            printf("%i Seconds, Distance: %f, Speed: %f m/s %f km/h %f MACH %f of light\n", i, distance_covered, speed, speed_kmh, speed_mach, speed_light);
+        //sleep(1);
     }
     return 0;
 }
