dbc377f6138a9c12215b83b323b88c000cdbb8e1
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-01-19 08:50:38 +0100

    Update animation.c

--- animation.c
+++ animation.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <time.h>
 
+#define FPS 25
+
 int main()
 {
 
-   struct timespec tim, tim2;
-   tim.tv_sec = 1;
-   tim.tv_nsec = 500000000L;
+    struct timespec tim, tim2;
+    tim.tv_sec = 0;
+    tim.tv_nsec = 1000000000 / FPS; /* sleep time in nanoseconds */
 
+    printf("\e[?25l"); /* hide cursor */
 
 	int i, j;
 	for (i = 0; i < 125; i++) {
 		for (j = 0; j < i; j++)
 			printf("\b");
+		for (j = 0; j < i; j++)
+			printf(" ");
 		printf("A");
-   if(nanosleep(&tim , &tim2) < 0 )   
-   {
-      printf("Nano sleep system call failed \n");
-      return -1;
-   }
-
-   printf("Nano sleep successfull \n");
+        fflush(stdout); /* otherwise printf is buffered until the loop has finished */
+        nanosleep(&tim , &tim2);
 	}
+    printf("\e[?25h"); /* unhide cursor */
 	return 0;
 }
