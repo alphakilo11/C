e425d1f1790640688150385d3c05c1e3286ef34f
Alexander <krendl.alexander@gmail.com>
2022-02-08 17:42:39 +0100

    qol

--- fragmente/create_file.c
+++ fragmente/create_file.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
     int i;
     long c, x;
     for (i = 0; i < 10000; i++) {
-        x = random() % 5;
+        x = random() % 65536;
         if (c == x)
             ;
         else {
-            printf("%li;", x);
+            printf("%li:%li;", time(0), x);
             c = x;
         }
     }
