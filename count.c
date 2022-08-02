ee87cdf40d5ea7c2f611ea15fb050d5a8b6d3596
Alexander <krendl.alexander@gmail.com>
2022-08-02 22:24:26 +0200

    improved

--- count.c
+++ count.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <time.h>
 
-#define REPETITIONS 23000000000
+#define REPETITIONS 16428571420
 
 int main() {
     time_t start_time = time(NULL);
-    for (long i = 0; i < REPETITIONS; i++) {
-        ;
+    long i = 0;
+    while (i < REPETITIONS){
+        i++;
     }
     time_t now = time(NULL);
-    double passed_time = difftime(now, start_time);
+    int passed_time = difftime(now, start_time);
     double cpu_time = (double) clock() / CLOCKS_PER_SEC;
-    printf("%li, %f, ", REPETITIONS, cpu_time);
-    printf("%f, ", passed_time);
+    printf("%li repetitions, %f CPU-Time, ", REPETITIONS, cpu_time);
+    printf("Time passed: %i s, Frequency: %li Hz", passed_time, i / passed_time);
     printf("%li\n", now);
     return 0;
 }
