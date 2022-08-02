6b44eaf74f97e62e1fee8421547ed1857a37cc61
Alexander <krendl.alexander@gmail.com>
2022-08-02 06:04:40 +0200

    bla

---
+++ primes.c
@@ -0,0 +1,27 @@
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+
+int is_prime(long number) {
+    // only works with odd numbers > 2
+    unsigned int i = 3;
+    while (i <= pow(number, 0.5)) {
+        if (number % i == 0) {
+            return 0;
+        };
+        i += 2;
+    };
+    return 1;
+};
+
+int main() {
+    unsigned int i = 3;
+    unsigned int count = 0;
+    while (count < 1000000) {
+        if (is_prime(i)) {
+            count++;
+        };
+        i += 2;
+    };
+    return 0;
+};
