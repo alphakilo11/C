b7c9169f6d3cff0b8b989e5b0de223e8c14077a5
Alexander <krendl.alexander@gmail.com>
2022-08-04 11:44:41 +0200

    dunno

--- primes.c
+++ primes.c
@@ -17,11 +17,14 @@ int is_prime(long number) {
 int main() {
     unsigned int i = 3;
     unsigned int count = 0;
-    while (count < 1000000) {
+    unsigned int result;
+    while (count < 999999) { // 2 is not considered for optimisation
         if (is_prime(i)) {
             count++;
+            result = i;
         };
         i += 2;
     };
+    printf("%i\n", result);
     return 0;
 };
